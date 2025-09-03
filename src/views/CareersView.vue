692ab46624277fd9c59866e26ee0f318ef9462a5
KevinCytonn <kgakio@cytonn.com>
2025-09-03 08:46:11 +0300

    initial commit

--- src/views/CareersView.vue
+++ src/views/CareersView.vue
@@ -1,5 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="">this is the career page</div>
+  <div class="max-w-full">
+    <div class="h-[200px] bg-[url('https://www.maliadvocatesllp.com/_nuxt/law-books.DnsjHjPE.png')] bg-cover bg-center"></div>
+  </div>
 </template>
