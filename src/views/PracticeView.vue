6ffeed112dab5143172b33a1a79de25ee63af4a3
DonaldEdwin-C <dbosco@cytonn.com>
2025-09-04 09:38:04 +0300

    commit

--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Articles from '@/components/Articles.vue'
 import ContactUs from '@/components/ContactUs.vue'
+import Values from '@/components/Values.vue'
 </script>
 
 <template>
   <div class="">this is the Practise</div>
+  <Values />
   <Articles />
   <ContactUs />
 </template>
